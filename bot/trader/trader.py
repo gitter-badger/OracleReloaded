73e205e2e496b0a6af647f7423132de790e40355
luserazo <serazo.luis@gmail.com>
2017-09-25 11:54:08 -0400

    test

---
+++ bot/trader/trader.py
@@ -0,0 +1,31 @@
+'''
+The internals of the trader. Defines a core structure 
+called Trader which aggregates strategies and manages
+behavior of the bot.
+'''
+
+__author__ = 'Luis Serazo'
+__email__ = 'serazo.luis@gmail.com'
+
+import math
+
+
+class Trader(object):
+
+	'''
+	class Trader
+	'''
+
+	def __init__(self):
+
+		'''
+		args:
+		'''
+
+		self.name = 'bot'
+
+
+
+	def say_hello(self):
+		print ("hello")
+
